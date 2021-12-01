61aafbe8433e5fdb5bfc31ae0b59d0217f379635
Serhat Pehlivan <serhat9703@hotmail.com>
2021-12-01 11:48:32 +0300

    mark6-hızlıveyavaşkoşma

--- TheProjectLake/Assets/Scripts/Controller.cs
+++ TheProjectLake/Assets/Scripts/Controller.cs
@@ -8,6 +8,9 @@ public class Controller : MonoBehaviour
     float inputX;
     float inputY;
 
+    float normalFov;
+    public float sprintFov;
+
     public Transform Player;
 
     Animator anim;
@@ -21,12 +24,17 @@ public class Controller : MonoBehaviour
     [Range(1,20)]
     public float rotationSpeed;
 
+    float maxSpeed;
+
+    public KeyCode SprintButton = KeyCode.LeftShift;
+    public KeyCode WalkButton = KeyCode.LeftControl;
 
 
     void Start()
     {
         anim = GetComponent<Animator>();
         mainCam = Camera.main;
+        normalFov = mainCam.fieldOfView;
 
     }
 
@@ -34,21 +42,54 @@ public class Controller : MonoBehaviour
 
     private void LateUpdate()
     {
-        inputX = Input.GetAxis("Horizontal");
-        inputY = Input.GetAxis("Vertical");
-        //karakterin gideceði yön için
-        StickDirection = new Vector3(inputX, 0, inputY);
+     
 
         InputMove();
         InputRotation();
+        Movement();
+
+    }
+
+    void Movement()
+    {
+        //yön belirlemek için
+        StickDirection = new Vector3(inputX, 0, inputY);
+
+        if (Input.GetKey(SprintButton))
+        {
+            //kamerayý uzaklaþtýrma,koþma efekti için
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, Time.deltaTime * 2);
+
+            maxSpeed = 1.5f;
+            inputX =2 * Input.GetAxis("Horizontal");
+            inputY =2 * Input.GetAxis("Vertical");
+        }
+
+        else if(Input.GetKey(WalkButton))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 0.3f;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+        }
+
+        else
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 1f;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+        }
 
 
+       
+        
     }
 
     void InputMove()
     {
         //hýzýn eksi olmasý yerine 0-1 arasýnda kalmasý için
-        anim.SetFloat("speed", Vector3.ClampMagnitude(StickDirection, 1).magnitude, damp , Time.deltaTime*10);
+        anim.SetFloat("speed", Vector3.ClampMagnitude(StickDirection, maxSpeed).magnitude, damp , Time.deltaTime*10);
     }
 
     void InputRotation()
