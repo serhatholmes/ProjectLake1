ffb742d3d38b4ff0daa8951f9a4e312969df45cd
Serhat Pehlivan <serhat9703@hotmail.com>
2021-12-09 12:27:42 +0300

    Mark8.5CombatDeneme

---
+++ TheProjectLake/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    bool isStafe = false;
+    Animator anim;
+
+    public GameObject handWeapon;
+    public GameObject sideWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            isStafe = !isStafe;
+        }
+    }
+
+    void equip()
+    {
+        sideWeapon.SetActive(false);
+        handWeapon.SetActive(true);
+    }
+    void unequip()
+    {
+        sideWeapon.SetActive(true);
+        handWeapon.SetActive(false);
+    }
+}
